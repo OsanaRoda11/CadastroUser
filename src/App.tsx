d2d106b5dbdb625da7549ec81e0c352b2081bc69
Osana Manuel <osanaroda00@gmail.com>
2025-05-06 16:39:45 +0100

    Actualizações

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  
+  return(
+    <div>
+      <form action="">
+        <input type="text" name='nome' />
+        <input type="number" name='idade' />
+        <input type="email" name='email' />
+        <button type='button'>Cadastrar</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
